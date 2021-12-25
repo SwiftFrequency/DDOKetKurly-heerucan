156eeea05b45c6d4839e91ce5f21f7946940d79a
heerucan <iwpow0307@gmail.com>
2021-12-26 07:01:55 +0900

    [Feat] SupplementaryView 구현(#4) - NSCollectionLayoutBoundarySupplementaryItem 사용해서 구현

--- SF-DDoketkurly/SF-DDoketkurly/Source/View/TitleSupplementaryView.swift
+++ SF-DDoketkurly/SF-DDoketkurly/Source/View/TitleSupplementaryView.swift
@@ -7,14 +7,39 @@
 
 import UIKit
 
-class TitleSupplementaryView: UIView {
+import SnapKit
+import Then
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class TitleSupplementaryView: UICollectionReusableView {
+    
+    static let identifier = "TitleSupplementaryView"
+    
+    // MARK: - Properties
+    
+    public let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 17, weight: .semibold)
+        $0.text = "이 상품 어때요?"
+        $0.textAlignment = .left
     }
-    */
-
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI & Layout
+    
+    private func configure() {
+        addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(33)
+            make.leading.equalToSuperview().inset(5)
+        }
+    }
 }
